0153de770a3d59aed02e75285a65ba5dc499bc96
Vitor Hirota <vitor.hirota@gmail.com>
2017-10-30 02:54:59 -0200

    adicionado endpoint escola/cod_eol/cardapios/data, e endpoint para atualizar os dados das escolas

--- api.py
+++ api.py
@@ -10,9 +10,9 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 API_KEY = os.environ.get('API_KEY')
-MONGO_URI = 'mongodb://{}'.format(os.environ.get('API_MONGO_URI'))
+API_MONGO_URI = 'mongodb://{}'.format(os.environ.get('API_MONGO_URI'))
 
-client = MongoClient(MONGO_URI)
+client = MongoClient(API_MONGO_URI)
 try:
     # The ismaster command is cheap and does not require auth.
     client.admin.command('ismaster')
@@ -80,72 +80,101 @@ def get_detalhe_escola(id_escola):
     return response
 
 
-@app.route('/cardapios')
-@app.route('/cardapios/<data>')
-def get_cardapios(data=None):
-    if request.method == 'GET':
+@app.route('/escola/<int:id_escola>/cardapios')
+@app.route('/escola/<int:id_escola>/cardapios/<data>')
+def get_cardapio_escola(id_escola, data=None):
+    escola = db.escolas.find_one({'_id': id_escola}, {'_id': False})
+    if escola:
         query = {
-            'status': 'PUBLICADO'
+            'status': 'PUBLICADO',
+            'agrupamento': str(escola['agrupamento']),
+            'tipo_atendimento': escola['tipo_atendimento'],
+            'tipo_unidade': escola['tipo_unidade']
         }
 
-        if request.args.get('agrupamento'):
-            query['agrupamento'] = request.args['agrupamento']
-        if request.args.get('tipo_atendimento'):
-            query['tipo_atendimento'] = request.args['tipo_atendimento']
-        if request.args.get('tipo_unidade'):
-            query['tipo_unidade'] = request.args['tipo_unidade']
         if request.args.get('idade'):
             query['idade'] = request.args['idade']
 
-        data_range = {}
-        if request.args.get('data_inicial'):
-            data_range.update({'$gte': request.args['data_inicial']})
-        if request.args.get('data_final'):
-            data_range.update({'$lte': request.args['data_final']})
-        if data or data_range:
-            query['data'] = data or data_range
-
-        limit = int(request.args.get('limit', 0))
-        page = int(request.args.get('page', 0))
+        if data:
+            query['data'] = str(data)
+        else:
+            data = {}
+            if request.args.get('data_inicial'):
+                data.update({'$gte': request.args['data_inicial']})
+            if request.args.get('data_final'):
+                data.update({'$lte': request.args['data_final']})
+            if data:
+                query['data'] = data
 
         fields = {
             '_id': False,
             'status': False,
-            'cardapio_original': False,
+            'cardapio_original': False
         }
 
         cardapios = db.cardapios.find(query, fields).sort([('data', -1)])
-        if page and limit:
-            cardapios = cardapios.skip(limit*(page-1)).limit(limit)
-        elif limit:
-            cardapios = cardapios.limit(limit)
 
         response = app.response_class(
             response=json_util.dumps(cardapios),
             status=200,
             mimetype='application/json'
         )
-        return response
-
-
-@app.route('/cardapio/<_id>')
-def get_cardapio(_id):
-    query = {
-        '_id': ObjectId(_id)
-    }
-    cardapio = db.cardapios.find(query)
-    if cardapio:
-        response = app.response_class(
-            response=json_util.dumps(cardapio),
-            status=200,
-            mimetype='application/json'
-        )
     else:
         response = app.response_class(
-            response=json_util.dumps({'erro': 'Cardapio inexistente'}),
+            response=json_util.dumps({'erro': 'Escola inexistente'}),
             status=404,
             mimetype='application/json'
         )
+    return response
+
+
+@app.route('/cardapios')
+@app.route('/cardapios/<data>')
+def get_cardapios(data=None):
+    query = {
+        'status': 'PUBLICADO'
+    }
+
+    if request.args.get('agrupamento'):
+        query['agrupamento'] = request.args['agrupamento']
+    if request.args.get('tipo_atendimento'):
+        query['tipo_atendimento'] = request.args['tipo_atendimento']
+    if request.args.get('tipo_unidade'):
+        query['tipo_unidade'] = request.args['tipo_unidade']
+    if request.args.get('idade'):
+        query['idade'] = request.args['idade']
+
+    if data:
+        query['data'] = data
+    else:
+        data = {}
+        if request.args.get('data_inicial'):
+            data.update({'$gte': request.args['data_inicial']})
+        if request.args.get('data_final'):
+            data.update({'$lte': request.args['data_final']})
+        if data:
+            query['data'] = data
+
+    limit = int(request.args.get('limit', 0))
+    page = int(request.args.get('page', 0))
+
+    fields = {
+        '_id': False,
+        'status': False,
+        'cardapio_original': False,
+    }
+
+    cardapios = db.cardapios.find(query, fields).sort([('data', -1)])
+    if page and limit:
+        cardapios = cardapios.skip(limit*(page-1)).limit(limit)
+    elif limit:
+        cardapios = cardapios.limit(limit)
+
+    response = app.response_class(
+        response=json_util.dumps(cardapios),
+        status=200,
+        mimetype='application/json'
+    )
     return response
 
 
@@ -205,6 +234,28 @@ def get_cardapios_editor():
                 bulk.insert(item)
         bulk.execute()
         return ('', 200)
+
+
+@app.route('/editor/escola/<int:id_escola>', methods=['POST'])
+def edit_escola(id_escola):
+    key = request.headers.get('key')
+    if key != API_KEY:
+        return ('', 401)
+
+    try:
+        payload = json_util.loads(request.data)
+    except:
+        return app.response_class(
+            response=json_util.dumps({'erro': 'Dados POST não é um JSON válido'}),
+            status=500,
+            mimetype='application/json'
+        )
+
+    db.escolas.update_one(
+        { '_id': id_escola },
+        { '$set': payload },
+        upsert=False)
+    return ('', 200)
 
 
 if __name__ == '__main__':
