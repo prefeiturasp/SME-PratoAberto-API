6ec7897c35417b9e37aed426ba446d46bffe593f
IagoCarvalho <IagoVC_2012@hotmail.com>
2018-09-18 20:48:04 -0300

    Adicionando validacao de email e respostas http
    Co-authored-by: Breno <mrzbeo@gmail.com>

--- users.py
+++ users.py
@@ -1,7 +1,6 @@
 import os
 
-from flask import request, redirect
-from flask import Blueprint
+from flask import make_response, request, jsonify, Blueprint
 from pymongo import MongoClient
 from werkzeug.security import generate_password_hash
 
@@ -21,9 +20,18 @@ def criar_usuario():
     email = request.json['email']
     senha = request.json['senha']
 
-    hs_senha = generate_password_hash(senha, "sha256")
+    # Valida email de acordo com padrao da prefeitura
+    dominio = email.split("@")[1]
+    if (dominio != "sme.prefeitura.sp.gov.br"):
+        response = make_response(jsonify({'HTTP': '406'}), 406)
 
-    usuario = {'email': email, 'senha': hs_senha}
-    db.usuarios.insert_one(usuario)
+    else:
+        # Criptografa senha
+        hs_senha = generate_password_hash(senha, "sha256")
 
-    return redirect('/')
+        usuario = {'email': email, 'senha': hs_senha}
+        db.usuarios.insert_one(usuario)
+
+        response = make_response(jsonify({'HTTP': '201'}), 201)
+
+    return response
